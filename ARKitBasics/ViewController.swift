1d5ff7f9cd890931dd4a44160fb94c9569283986
Mikael Andersson <mikael.andersson@volumental.com>
2017-12-19 13:54:25 +0100

    Draws box and checks distance to box

--- ARKitBasics/ViewController.swift
+++ ARKitBasics/ViewController.swift
@@ -26,6 +26,8 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 	// MARK: - View Life Cycle
 	let manager = CMMotionManager()
     var plane_found = false
+    var cube: SCNBox = SCNBox()
+    var cubeNode: SCNNode = SCNNode()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,6 +52,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
                 self?.rotX.text = String(format: "rotX: %.2f", data!.gravity.x)
                 self?.rotY.text = String(format: "rotY: %.2f", data!.gravity.y)
                 self?.rotZ.text = String(format: "rotZ: %.2f", data!.gravity.z)
+                // Rewrite as explicit state machine
                 if capture_complete || !(self?.plane_found)! {
                     if UInt64(NSDate().timeIntervalSince1970 * 1000.0) > time_started_capture_complete + 2000 {
                         capture_complete = false
@@ -112,11 +115,16 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
          device position and orientation tracking, and plane detection.
         */
         let configuration = ARWorldTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
         configuration.planeDetection = .horizontal
         sceneView.session.run(configuration)
 
         // Set a delegate to track the number of plane anchors for providing UI feedback.
         sceneView.session.delegate = self
+       
+        sceneView.autoenablesDefaultLighting = true
+        sceneView.automaticallyUpdatesLighting = true
+        //sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
         
         /*
          Prevent the screen from being dimmed after a while as users will likely
@@ -125,7 +133,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         UIApplication.shared.isIdleTimerDisabled = true
         
         // Show debug UI to view performance metrics (e.g. frames per second).
-        sceneView.showsStatistics = true
+        sceneView.showsStatistics = false
         
     }
 
@@ -159,8 +167,6 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
 	}
 	
 	// MARK: - ARSCNViewDelegate
-    
-    /// - Tag: PlaceARContent
 	func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         // Place content only for anchors found by plane detection.
         guard let planeAnchor = anchor as? ARPlaneAnchor else { return }
@@ -177,18 +183,28 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         planeNode.eulerAngles.x = -.pi / 2
         
         // Make the plane visualization semitransparent to clearly show real-world placement.
-        planeNode.opacity = 0.25
+        planeNode.opacity = 0.05
+        
         
         /*
          Add the plane visualization to the ARKit-managed node so that it tracks
          changes in the plane anchor as plane estimation continues.
         */
-        node.addChildNode(planeNode)
+        if !plane_found {
+            cube = SCNBox(width: 0.1, height: 0.2, length: 0.3, chamferRadius: 0.03)
+            // ambient, diffuse, specular, and shininess
+            cube.firstMaterial?.lightingModel = SCNMaterial.LightingModel.phong
+            cube.firstMaterial?.diffuse.contents = UIColor(red: 0.0, green: 0.0, blue: 0.0, alpha: 0.5)
+            cube.firstMaterial?.shininess = 0.5
+            cubeNode = SCNNode(geometry: cube)
+            cubeNode.position = SCNVector3(planeAnchor.center.x, planeAnchor.center.y+Float(cube.height/2.0), planeAnchor.center.z)
+            node.addChildNode(cubeNode)
+            node.addChildNode(planeNode)
+            // Vibrate when plane found
+            plane_found = true
+            AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
+        }
         
-        // Vibrate when plane found
-        plane_found = true
-        AudioServicesPlayAlertSound(SystemSoundID(kSystemSoundID_Vibrate))
-
 	}
 
     /// - Tag: UpdateARContent
@@ -210,10 +226,21 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate {
         */
         plane.width = CGFloat(planeAnchor.extent.x)
         plane.height = CGFloat(planeAnchor.extent.z)
+        
     }
 
     // MARK: - ARSessionDelegate
-
+    func session(_ session: ARSession, didUpdate frame: ARFrame) {
+        let transform = SCNMatrix4(frame.camera.transform)
+        let location = SCNVector3(transform.m41, transform.m42, transform.m43)
+        let distance = location.distance(vector: cubeNode.convertPosition(cubeNode.position, to: nil))
+        if (distance < 0.8) {
+            self.cube.firstMaterial?.diffuse.contents = UIColor(red: 0.0, green: 1.0, blue: 0.0, alpha: 0.5)
+        } else {
+            self.cube.firstMaterial?.diffuse.contents = UIColor(red: 1.0, green: 0.0, blue: 0.0, alpha: 0.5)
+        }
+    }
+    
     func session(_ session: ARSession, didAdd anchors: [ARAnchor]) {
         guard let frame = session.currentFrame else { return }
         updateSessionInfoLabel(for: frame, trackingState: frame.camera.trackingState)
